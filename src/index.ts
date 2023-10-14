914af4af1186ea6b9ecaef7a64b4df84bebd89f9
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-10-14 04:20:53 +0000

    Create express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log(process.env.PORT)
+import express, { Application, Request, Response } from "express"
+
+const app: Application = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
